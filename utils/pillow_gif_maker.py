fc9d2212cef73b428f16387b23d4a58d26c0fec6
Viktor Aleksandrovsky <106704143+AleksandrovskyV@users.noreply.github.com>
2026-07-11 10:44:59 +0300

    Create pillow_gif_maker.py

---
+++ utils/pillow_gif_maker.py
@@ -0,0 +1,53 @@
+# pillow gif maker 
+# support any length size 
+
+import os
+from PIL import Image
+
+source_folder = "./cursors"
+gif_name = "animation_64_colors.gif" 
+
+# target_ms
+# 40 мс = 25 FPS
+# 33 мc = 30 FPS
+
+target_ms = 33
+loop_count = 0
+target_color = 64 
+
+print(f"Collect png images...")
+
+png_files = sorted(
+    [f for f in os.listdir(source_folder) if f.lower().endswith(".png")]
+)
+
+if not png_files:
+    print("In targe folder no png images!")
+    exit()
+
+print(f"Collect png images: {len(png_files)}")
+
+frames = []
+
+for i, file_name in enumerate(png_files):
+    file_path = os.path.join(source_folder, file_name)
+    img = Image.open(file_path).convert("RGB")
+
+    img_64_colors = img.quantize(colors=target_color, dither=0)
+    frames.append(img_64_colors)
+
+    if (i + 1) % 100 == 0:
+        print(f"Completed: {i + 1} frames...")
+
+print("Making gif by pillow...")
+
+frames[0].save(
+    gif_name,
+    save_all=True,
+    append_images=frames[1:],
+    duration=target_ms,
+    loop=loop_count,
+    optimize=False,
+)
+
+print(f"Succes! File save as: {gif_name}")
